d7c0757d73e7e3fa45e9ddb682ad09d7df36d98e
German Popoter <g.popoteur@gmail.com>
2014-02-19 09:45:15 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sigco::Application.config.secret_key_base = '13d3ea59e10fb4afdbe8d5c1c5ea962e38fe5996add25608892d3c720683541757cb5fa753d11646725af5b7cd0a1be8e451cd01659fc09ca9f3ddf07f79d842'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sigco::Application.config.session_store :cookie_store, key: '_sigco_session'
